7ec8780a9eaf0745cfc153c80e0fe53a00643bbb
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-08-08 20:15:47 -0300

    Subi el proyecto base

---
+++ src/main/java/com/Nahudev/shoppingCart_service_apiRest/ShoppingCartServiceApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.Nahudev.shoppingCart_service_apiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartServiceApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartServiceApiRestApplication.class, args);
+	}
+
+}
